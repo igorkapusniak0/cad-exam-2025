342c3eaae86bd9132447dd80edc0698aaabfdc75
igor <20102236@mail.wit.ie>
2025-12-11 10:52:35 +0000

    Updated add stock processing

--- lambdas/lambdaA.ts
+++ lambdas/lambdaA.ts
@@ -2,17 +2,21 @@
 import { SQSHandler } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
-import { DBAuctionItem , AuctionItem } from "../shared/types";
+import { DBAuctionItem , AuctionItem, AuctionType } from "../shared/types";
 const ddbDocClient = createDDbDocClient();
 
 export const handler: SQSHandler = async (event) => {
   console.log("Event ", JSON.stringify(event));
 
   for (const record of event.Records) {
-    const auctionItem = JSON.parse(record.body) as AuctionItem;
+    const data = JSON.parse(record.body)
+    const message = JSON.parse(data.Message);
+    const auctionItem = message as AuctionItem;
+    const auctionType = message.MessageAttributes?.auction_type?.Value;
+    
     const dbItem: DBAuctionItem = {
       ...auctionItem,
-      auctionType: "Public",  // Hardcoded for now.
+      auctionType,
     }
     await ddbDocClient.send(
       new PutCommand({
